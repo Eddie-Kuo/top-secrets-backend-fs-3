9809af5267b0471e68e61af385f793937acd6f33
Eddie-Kuo <Eddiekuo97@gmail.com>
2022-11-16 18:55:09 -0600

    SQL table and file setup

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -7,9 +7,19 @@ describe('backend-express-template routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
-  it('example test - delete me!', () => {
-    expect(1).toEqual(1);
-  });
+
+  // create a mock user with all the user's data that we want
+
+  // create a variable of the registered and logged in user?
+  //* asynchronous - pass in userProps as an empty object
+  // here we can create an agent that allows us to store cookies between each request in a test
+  // then create a user to sign in with
+  //* user = await user service .create({spread operator of mock and userProps})
+  // sign in by sending email and password
+  // return the agent and user
+
+  // now with the tests we can use the agent and mock user to test the routes
+
   afterAll(() => {
     pool.end();
   });
