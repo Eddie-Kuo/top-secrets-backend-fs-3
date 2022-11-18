388b64a50a1c9fce3e1d9868edb9b83615d3dbdf
Eddie-Kuo <Eddiekuo97@gmail.com>
2022-11-18 10:20:02 -0600

    edits on creat =e users test

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -4,45 +4,27 @@ const request = require('supertest');
 const app = require('../lib/app');
 const UserService = require('../lib/services/UserService');
 
-// create a mock user with all the user's data that we want
-const mockUser = {
-  firstName: 'first',
-  lastName: 'last',
-  email: 'user@testing.com',
-  password: '123456',
-};
-
-// create a function that signs in a user so that we can use it for testing
-const testingUser = async (userProps = {}) => {
-  const password = userProps.password ?? mockUser.password;
-
-  // the agent gives us the ability to store cookies for each request per test
-  const agent = request.agent(app);
-
-  // create a user that we can sign in with
-  // set up the user service file in order for us to create the user
-  // this file will also be in charge of signing in the user
-  const user = await UserService.create({ ...mockUser, ...userProps });
-  // sign in
-  const { email } = user;
-  await agent.post('/api/v1/users/sessions').send({ email, password });
-  return [agent, user];
-};
-
-// now with the tests we can use the agent and mock user to test the routes
-
 describe('backend-express-template routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
+  // create a mock user with all the user's data that we want
+  const mockUser = {
+    firstName: 'first',
+    lastName: 'last',
+    email: 'user@testing.com',
+    password: '123456',
+  };
   // testing route to make sure user is able to log in
-  test('/POST adds a new user', async () => {
+  test('/POST /api/v1/users adds a new user', async () => {
     const res = await request(app).post('/api/v1/users').send(mockUser);
     // destructure mock user to expect the same values from mock user
-    const { firstName, lastName } = mockUser;
+    const { firstName, lastName, email } = mockUser;
     expect(res.body).toEqual({
+      id: expect.any(String),
       firstName,
       lastName,
+      email,
     });
   });
 
